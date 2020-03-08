09fec99f553943b72ebb83c05b7c8fc4626f28f5
Luis Felipe Santos do Nascimento <luisfelipesdn12@gmail.com>
2020-03-08 20:38:17 -0300

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,6 @@
+credentials = {
+    'IBM' : 'YOUR_IBM_CLOUD_API_KEY',
+    'GCP' : {'my_api_key' : "YOUR_GOOGLE_CLOUD_PLATFORM_API_KEY",
+             'my_cse_id' : "YOUR_CUSTOM_SEARCH_ID"
+             }
+    }
